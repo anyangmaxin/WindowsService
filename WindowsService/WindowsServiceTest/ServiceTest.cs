6f8b7a4d3691c6b79e4e9f9f881034b3c571ee5d
马鑫 <anyangmaxin@163.com>
2015-10-24 10:27:11 +0800

    写入Service逻辑
    一个启动方法一个停止方法  启动写入日志  停止时也写入日志

--- WindowsService/WindowsServiceTest/ServiceTest.cs
+++ WindowsService/WindowsServiceTest/ServiceTest.cs
@@ -7,22 +7,40 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace WindowsServiceTest
 {
     public partial class ServiceTest : ServiceBase
     {
+        private string path = "D:\\WindowsService\\log.txt";
         public ServiceTest()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// 启动方法
+        /// </summary>
+        /// <param name="args"></param>
         protected override void OnStart(string[] args)
         {
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                sw.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")+" Start.");
+            }
         }
 
+
+        /// <summary>
+        /// 停止方法
+        /// </summary>
         protected override void OnStop()
         {
+            using (StreamWriter sw=new StreamWriter(path,true))
+            {
+                sw.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")+" Stop.");
+            }
         }
     }
 }
